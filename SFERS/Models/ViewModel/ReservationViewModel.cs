4fbff593d93ecbb04b77408d8b4d860e1b431dc1
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-07 13:24:57 +0800

    Moved designer models to its own view models

---
+++ SFERS/Models/ViewModel/ReservationViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SFERS.Models
+{
+    // --- RESERVATION MODELS ---
+    public class DashboardStatsViewModel
+    {
+        public int UpcomingReservations { get; set; }
+        public int CompletedBookings { get; set; }
+        public string? NextBookingTime { get; set; }
+        public List<ReservationViewModel>? RecentActivity { get; set; }
+    }
+
+    public class ReservationViewModel
+    {
+        public int Id { get; set; }
+        public string? RoomName { get; set; }
+        public DateTime Date { get; set; }
+        public string? TimeSlot { get; set; }
+        public string? Purpose { get; set; }
+        public string? Status { get; set; }
+        public string? EquipmentRequested { get; set; }
+    }
+}
--- SFERS/Models/ViewModel/RoomViewModel.cs
+++ SFERS/Models/ViewModel/RoomViewModel.cs
@@ -18,12 +18,4 @@ namespace SFERS.Models
         public string? AssignedRoom { get; set; } // Added ?
         public string? Status { get; set; } // Added ?
     }
-
-    public class UserProfileViewModel
-    {
-        public string? FullName { get; set; } // Added ?
-        public string? Role { get; set; } // Added ?
-        public string? Email { get; set; } // Added ?
-        public string? Username { get; set; } // Added ?
-    }
 }

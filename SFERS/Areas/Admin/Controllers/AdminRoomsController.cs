5e9469b406b115aa5d5f733e9f071360564074b3
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-10 14:52:54 +0800

    Added way to add rooms

--- SFERS/Areas/Admin/Controllers/AdminRoomsController.cs
+++ SFERS/Areas/Admin/Controllers/AdminRoomsController.cs
@@ -3,16 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SFERS.Data;
 using SFERS.Models.ViewModel;
+using SFERS.Models.Entities;
 
 namespace SFERS.Controllers.Admin
 {
     [Area("Admin")]
     [Authorize(Policy = "AdminOnly")]
-    public class RoomsController : Controller
+    public class AdminRoomsController : Controller
     {
         private ApplicationDbContext dbContext;
 
-        public RoomsController(ApplicationDbContext context)
+        public AdminRoomsController(ApplicationDbContext context)
         {
             dbContext = context;
         }
@@ -21,13 +22,12 @@ namespace SFERS.Controllers.Admin
         public async Task<IActionResult> Index()
         {
             var rooms = await dbContext.Rooms.ToListAsync();
-            var roomViewModels = new List<RoomViewModel>();
+            var roomViewModels = new AdminRoomViewModel();
 
-            // TODO: Fetch from Database
             foreach (var room in rooms)
             {
                 var equipment = await dbContext.Equipments.Where(e => e.RoomId == room.Id).ToListAsync();
-                roomViewModels.Add(new RoomViewModel
+                roomViewModels.Rooms.Add(new RoomViewModel
                 {
                     Id = room.Id,
                     Name = room.Name,
@@ -36,27 +36,26 @@ namespace SFERS.Controllers.Admin
                     Equipment = equipment.Select(e => e.Name).ToList()
                 });
             }
-            /*
-            var rooms = new List<RoomViewModel>
-            {
-                new RoomViewModel { Id = 1, Name = "Auditorium", Capacity = 200, IsAvailable = true, Equipment = new List<string>{"Projector", "Sound System"} },
-                new RoomViewModel { Id = 2, Name = "Lab 1", Capacity = 40, IsAvailable = false, Equipment = new List<string>{"Computers"} }
-            }; */
+
             return View(roomViewModels);
         }
 
-        // POST: /AdminRooms/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(RoomViewModel model)
+        public async Task<IActionResult> Create(AdminRoomViewModel model)
         {
-            if (ModelState.IsValid)
+            Console.WriteLine("Create Called");
+            if (!ModelState.IsValid)
             {
-                // TODO: _context.Rooms.Add(model);
-                // TODO: _context.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View("Index", model); // Return with errors
+            dbContext.Rooms.Add(new Room
+            {
+                Name = model.NewRoomName,
+                Capacity = model.NewRoomCapacity
+            });
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         // POST: /AdminRooms/Edit/5

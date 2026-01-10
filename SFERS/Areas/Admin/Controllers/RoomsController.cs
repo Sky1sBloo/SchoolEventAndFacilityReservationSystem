91523e07b69b7ca714067391b36f1f2ae5f059a7
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-10 10:57:04 +0800

    Added retrieve rooms in admin

--- SFERS/Areas/Admin/Controllers/RoomsController.cs
+++ SFERS/Areas/Admin/Controllers/RoomsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SFERS.Data;
 using SFERS.Models.ViewModel;
 
 namespace SFERS.Controllers.Admin
@@ -8,16 +10,39 @@ namespace SFERS.Controllers.Admin
     [Authorize(Policy = "AdminOnly")]
     public class RoomsController : Controller
     {
-        // GET: /AdminRooms
-        public IActionResult Index()
+        private ApplicationDbContext dbContext;
+
+        public RoomsController(ApplicationDbContext context)
         {
+            dbContext = context;
+        }
+
+        // GET: /AdminRooms
+        public async Task<IActionResult> Index()
+        {
+            var rooms = await dbContext.Rooms.ToListAsync();
+            var roomViewModels = new List<RoomViewModel>();
+
             // TODO: Fetch from Database
+            foreach (var room in rooms)
+            {
+                var equipment = await dbContext.Equipments.Where(e => e.RoomId == room.Id).ToListAsync();
+                roomViewModels.Add(new RoomViewModel
+                {
+                    Id = room.Id,
+                    Name = room.Name,
+                    Capacity = room.Capacity,
+                    IsAvailable = true,  // TODO: Add logic by checking reservation
+                    Equipment = equipment.Select(e => e.Name).ToList()
+                });
+            }
+            /*
             var rooms = new List<RoomViewModel>
             {
                 new RoomViewModel { Id = 1, Name = "Auditorium", Capacity = 200, IsAvailable = true, Equipment = new List<string>{"Projector", "Sound System"} },
                 new RoomViewModel { Id = 2, Name = "Lab 1", Capacity = 40, IsAvailable = false, Equipment = new List<string>{"Computers"} }
-            };
-            return View(rooms);
+            }; */
+            return View(roomViewModels);
         }
 
         // POST: /AdminRooms/Create

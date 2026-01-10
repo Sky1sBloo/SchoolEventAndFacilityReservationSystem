b1d2e54845b569b0135d900bdad87a81d27cd70b
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-10 10:57:51 +0800

    Removed warings for admin reservation view model

--- SFERS/Models/ViewModel/AdminReservationViewModel.cs
+++ SFERS/Models/ViewModel/AdminReservationViewModel.cs
@@ -3,10 +3,10 @@
     public class AdminReservationViewModel
     {
         public int Id { get; set; }
-        public string RoomName { get; set; }
+        public required string RoomName { get; set; }
         public DateTime Date { get; set; }
-        public string TimeSlot { get; set; }
-        public string Purpose { get; set; }
-        public string Status { get; set; }
+        public string? TimeSlot { get; set; }
+        public string? Purpose { get; set; }
+        public required string Status { get; set; }
     }
 }

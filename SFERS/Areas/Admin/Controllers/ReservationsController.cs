c8a9f1afdb24c84960e94459d029165415421e34
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-10 10:26:36 +0800

    Admin area and authoriuzation policy on admin reservations controller

--- SFERS/Areas/Admin/Controllers/ReservationsController.cs
+++ SFERS/Areas/Admin/Controllers/ReservationsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SFERS.Models.ViewModel;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFERS.Models.ViewModel; 
 
 namespace SFERS.Controllers.Admin
 {
+    [Area("Admin")]
+    [Authorize(Policy = "AdminOnly")]
     public class ReservationsController : Controller
     {
         // GET: /AdminReservations

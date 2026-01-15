97217ebd93bfb6fc74e05a3e83954196a9d7fa6d
Marlon <Marlon@MSI>
2026-01-16 05:35:50 +0800

    #8 user side

---
+++ SFERS/Models/ViewModel/ReservationCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFERS.Models.ViewModel
+{
+    public class ReservationCreateViewModel
+    {
+         {
+        [Required(ErrorMessage = "Please select a room.")]
+        public int RoomId { get; set; }
+
+        [Required(ErrorMessage = "Please select a date.")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Please enter a start time.")]
+        [DataType(DataType.Time)]
+        public TimeSpan StartTime { get; set; }
+
+        [Required(ErrorMessage = "Please enter an end time.")]
+        [DataType(DataType.Time)]
+        public TimeSpan EndTime { get; set; }
+
+        [Required(ErrorMessage = "Please provide the purpose.")]
+        [StringLength(500)]
+        public string? Purpose { get; set; }
+    }
+}
+
+
---
+++ SFERS/Models/ViewModel/RoomDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SFERS.Models.ViewModel
+{
+    public class RoomDetailsViewModel
+    {
+        public RoomViewModel Room { get; set; } = new RoomViewModel();
+        public List<ReservationViewModel> UpcomingReservations { get; set; } = new List<ReservationViewModel>();
+    }
+}

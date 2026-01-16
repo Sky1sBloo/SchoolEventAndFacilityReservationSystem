adea249c25add7ea5d4856c7159e5926791ff1fd
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-16 17:58:26 +0800

    Modified rooms to view upcoming reservations

--- SFERS/Models/ViewModel/RoomViewModel.cs
+++ SFERS/Models/ViewModel/RoomViewModel.cs
@@ -10,6 +10,7 @@ namespace SFERS.Models.ViewModel
 
         public int Capacity { get; set; }
         public bool IsAvailable { get; set; }
+        public List<string> Reservations { get; set; } = new List<string>();
 
         // Fix: Initialize the list so it is never null
         public List<string> Equipment { get; set; } = new List<string>();

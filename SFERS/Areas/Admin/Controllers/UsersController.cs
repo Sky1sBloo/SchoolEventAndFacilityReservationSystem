ce692dc50ea73f97ffaf588f74751cf9e61edce2
Sky1sBloo <ultimate8gaming@gmail.com>
2026-01-16 13:39:03 +0800

    Added way to reset user password

--- SFERS/Areas/Admin/Controllers/UsersController.cs
+++ SFERS/Areas/Admin/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SFERS.Data;
 using SFERS.Models.ViewModel;
+using SFERS.Utilities;
 
 namespace SFERS.Controllers.Admin
 {
@@ -14,23 +16,9 @@ namespace SFERS.Controllers.Admin
         {
             dbContext = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var users = dbContext.Accounts.ToList();
-            var userViewModel = new List<UserProfileViewModel>();
-            foreach (var user in users)
-            {
-                var role = dbContext.Roles.Find(user.RoleId);
-                userViewModel.Add(new UserProfileViewModel
-                {
-                    Id = user.Id,
-                    FullName = user.FullName,
-                    Email = user.Email,
-                    Role = role != null ? role.Name : "Unknown",
-                    IsActive = false
-                });
-            }
-            return View(userViewModel);
+            return View(await GetAllUsers());
         }
 
         // Promotes/Demotes a user (e.g., Student -> Admin)
@@ -53,12 +41,45 @@ namespace SFERS.Controllers.Admin
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(int userId)
+        {
+            var user = await dbContext.Accounts.FindAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.Password = PasswordManager.HashPassword("Default@1234");
+            await dbContext.SaveChangesAsync();
+            TempData["Message"] = $"Password for {user.FullName} has been reset to 'Default@1234'.";
+            return View("Index", await GetAllUsers());
+        }
+
         // Bans or Unbans a user
         [HttpPost]
         public IActionResult ToggleBan(int userId)
         {
             // TODO: Fetch user, toggle IsActive boolean
             return RedirectToAction("Index");
+        }
+
+        private async Task<List<UserProfileViewModel>> GetAllUsers()
+        {
+            var users = await dbContext.Accounts.ToListAsync();
+            var userViewModel = new List<UserProfileViewModel>();
+            foreach (var user in users)
+            {
+                var role = await dbContext.Roles.FindAsync(user.RoleId);
+                userViewModel.Add(new UserProfileViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                    Role = role != null ? role.Name : "Unknown",
+                    IsActive = false
+                });
+            }
+            return userViewModel;
         }
     }
 }
